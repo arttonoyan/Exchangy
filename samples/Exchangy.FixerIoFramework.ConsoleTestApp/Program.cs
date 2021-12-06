0c8009f4af548f95c1ad2b6b97b4fbbd4eb3cbc3
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-06 19:59:22 +0400

    Create solution

---
+++ samples/Exchangy.FixerIoFramework.ConsoleTestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exchangy.FixerIoFramework.ConsoleTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
