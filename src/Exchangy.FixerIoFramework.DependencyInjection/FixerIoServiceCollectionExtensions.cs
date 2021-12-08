f29b43c7116beb75d7e52e98ca1f9222b1548883
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-08 20:44:56 +0400

    Create DependencyInjection library for FixerIo client

---
+++ src/Exchangy.FixerIoFramework.DependencyInjection/FixerIoServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Exchangy.FixerIoFramework;
+using Polly;
+using Polly.Extensions.Http;
+using System;
+using System.Net.Http;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class FixerIoServiceCollectionExtensions
+    {
+        public static IServiceCollection AddFixerIoClient(this IServiceCollection services, Action<FixerOptions> optionsAction)
+        {
+            var options = new FixerOptions();
+            optionsAction.Invoke(options);
+            return services.AddFixerIoClient(options);
+        }
+
+        public static IServiceCollection AddFixerIoClient(this IServiceCollection services, FixerOptions options)
+        {
+            services
+                .AddSingleton(options)
+                .AddHttpClient<IFixerIoClient, FixerIoClient>((provider, client) =>
+                {
+                    var options = provider.GetService<FixerOptions>();
+                    client.BaseAddress = new Uri(options.BaseUrl);
+                });
+            //.AddPolicyHandler(GetRetryPolicy());
+            return services;
+        }
+
+        static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+                .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        }
+    }
+}
